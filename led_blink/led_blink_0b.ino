059a46c82ae086b47a84075d8c2d61e9bd1c454d
James Findley de Regt <84346184+jfdr-ucsd@users.noreply.github.com>
2021-05-17 12:34:03 -0700

    Add files via upload

---
+++ led_blink/led_blink_0b.ino
@@ -0,0 +1,22 @@
+/*
+ * Expanding the blink program beyond the onboard LED
+ * or pin 13, which is tied to the onboard LED
+ */
+
+// define the LED pin
+
+int led = 8;
+
+
+void setup() {
+  // initialize the chosen LED pin as an output
+  pinMode(led, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
